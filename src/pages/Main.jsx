54057e0be9e94497113c36d6e7a19ad7a0063871
Alexey Ukrainets <alekseyukrainets@MacBook-Pro-Alexey.local>
2022-12-23 01:14:14 +0300

    token card end, but need some fix

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import css from '../styles/pages/main.module.scss';
+import CardToken from '../ui/CardToken/CardToken';
 // import MainLayout from '../ui/MainLayout/MainLayout';
 import Tabs from '../ui/Tabs/Tabs';
+import testLogo from '../assets/test_logo_token.svg'
+
 
 const tabs = [
   { id: 1, title: 'ANALYZED' },
@@ -12,10 +15,138 @@ const tabs = [
 
 export default function Main() {
   const [tab, setTab] = useState(0);
+  const [cards, setCards] = useState([
+    {
+      id: 1,
+      type: 'pro',
+      text: 'analyzed',
+      rarity: 1,
+      logo: testLogo,
+      title: 'Trust Wallet',
+      desc: 'Token with 500.000$ marketcap. and 200.000$ liquidity',
+      rise: '45%',
+      holders: '1500',
+      age: '2d'
+    },
+    {
+      id: 2,
+      type: 'pro',
+      text: 'analyzed',
+      rarity: 1,
+      logo: testLogo,
+      title: 'Xavier token',
+      desc: 'Founded token for new walletts and transactions. Analyzed for...',
+      rise: '45%',
+      holders: '1500',
+      age: '2d'
+    },
+    {
+      id: 3,
+      type: 'pro',
+      text: 'analyzed',
+      rarity: 1,
+      logo: testLogo,
+      title: 'Car rent token',
+      desc: 'Token with 500.000$ marketcap. and 200.000$ liquidity ',
+      rise: '45%',
+      holders: '1500',
+      age: '2d'
+    },
+    {
+      id: 4,
+      type: 'pro',
+      text: 'analyzed',
+      rarity: 1,
+      logo: testLogo,
+      title: 'Omona coin',
+      desc: 'Token with 500.000$ marketcap. and 200.000$ liquidity',
+      rise: '45%',
+      holders: '1500',
+      age: '2d'
+    },
+    {
+      id: 4,
+      type: 'pro',
+      text: 'analyzed',
+      rarity: 1,
+      logo: testLogo,
+      title: 'Omona coin',
+      desc: 'Token with 500.000$ marketcap. and 200.000$ liquidity',
+      rise: '45%',
+      holders: '1500',
+      age: '2d'
+    },
+    {
+      id: 4,
+      type: 'pro',
+      text: 'analyzed',
+      rarity: 1,
+      logo: testLogo,
+      title: 'Omona coin',
+      desc: 'Token with 500.000$ marketcap. and 200.000$ liquidity',
+      rise: '45%',
+      holders: '1500',
+      age: '2d'
+    },
+    {
+      id: 4,
+      type: 'pro',
+      text: 'analyzed',
+      rarity: 1,
+      logo: testLogo,
+      title: 'Omona coin',
+      desc: 'Token with 500.000$ marketcap. and 200.000$ liquidity',
+      rise: '45%',
+      holders: '1500',
+      age: '2d'
+    },
+    {
+      id: 4,
+      type: 'pro',
+      text: 'analyzed',
+      rarity: 1,
+      logo: testLogo,
+      title: 'Omona coin',
+      desc: 'Token with 500.000$ marketcap. and 200.000$ liquidity',
+      rise: '45%',
+      holders: '1500',
+      age: '2d'
+    },
+    {
+      id: 4,
+      type: 'pro',
+      text: 'analyzed',
+      rarity: 1,
+      logo: testLogo,
+      title: 'Omona coin',
+      desc: 'Token with 500.000$ marketcap. and 200.000$ liquidity',
+      rise: '45%',
+      holders: '1500',
+      age: '2d'
+    },
+    {
+      id: 4,
+      type: 'pro',
+      text: 'analyzed',
+      rarity: 1,
+      logo: testLogo,
+      title: 'Omona coin',
+      desc: 'Token with 500.000$ marketcap. and 200.000$ liquidity',
+      rise: '45%',
+      holders: '1500',
+      age: '2d'
+    },
+   
+  ]);
   return (
-    <div className={css.main} >
+    <div className={css.main}>
       <h1 className={css.title}>{tabs[tab].title}</h1>
       <Tabs tabs={tabs} initialTab={tab} onTabChange={setTab} />
+      {tab === 0 ? <div className={css.cards}>
+        {cards.map((el, index) => {
+          return <CardToken key={index} data={el} />;
+        })}
+      </div> : <></>}
     </div>
   );
 }
---
+++ src/ui/CardToken/CardToken.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import css from './style.module.scss';
+export default function CardToken({ data }) {
+  return (
+    <div className={css.card}>
+      <div className={css.header}>
+        <div className={css.type}>{data?.type}</div>
+        <div className={css.text}>{data?.text}</div>
+        <div className={css.rarity}></div>
+      </div>
+      <div className={css.logo}>
+        <img src={data?.logo} alt="logo" />
+      </div>
+      <div className={css.title}>{data.title}</div>
+      <div className={css.desc}>{data.desc}</div>
+      <div className={css.tokenInfo}>
+        <div className={css.rise}>
+          <div>Rise</div>
+          <div>{data?.rise}</div>
+        </div>
+        <div className={css.holders}>
+          <div>Holders</div>
+          <div> {data?.holders}</div>
+        </div>
+        <div className={css.age}>
+            <div>Age</div>
+            <div>{data.age}</div>
+        </div>
+      </div>
+      <button className={css.btn}>more info</button>
+    </div>
+  );
+}
