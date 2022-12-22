514a9e19b5397410ab5ac1419649d41d34dae97e
Aratmany <arat.mda@gmail.com>
2022-12-23 00:40:27 +0300

    add variables

--- src/ui/ContainerBox/ContainerBox.jsx
+++ src/ui/ContainerBox/ContainerBox.jsx
@@ -1,5 +1,10 @@
 import Paper from '@mui/material/Paper';
+import css from '../../styles/ui/containerBox.module.scss';
 
 export const ContainerBox = ({ children }) => {
-  return <Paper sx={{ p: 2 }}>{children}</Paper>;
+  return (
+    <Paper sx={{ p: 2, borderRadius: 2.5 }} className={css.containerBox}>
+      {children}
+    </Paper>
+  );
 };
