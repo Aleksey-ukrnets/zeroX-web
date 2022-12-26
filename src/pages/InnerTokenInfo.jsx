62a8a206ca8151ac6a9c3e0402eb5449d99c51c5
Aratmany <arat.mda@gmail.com>
2022-12-27 01:39:28 +0300

    move TwitterCard to CommonInnerTokenInfo

--- src/pages/InnerTokenInfo.jsx
+++ src/pages/InnerTokenInfo.jsx
@@ -1,9 +1,29 @@
+import { Box } from '@mui/material';
 import { TokenInfo } from '../ui/TokenInfo/TokenInfo';
+import TwitterCard from '../ui/TwitterCard/TwitterCard';
+
+import { useSelector } from 'react-redux';
+import { twitterCardsSelector } from '../store/slices/twitterCards';
 
 export const InnerTokenInfo = () => {
+  const { twitterCards } = useSelector(twitterCardsSelector.getTwitterCards);
   return (
     <main className="mt-16 sm:mt-24 ">
       <TokenInfo />
+      {/* -------------- Twitter -------------- */}
+      <Box
+        sx={{
+          display: 'flex',
+          overflowX: 'scroll',
+          gap: '20px',
+          padding: '20px 0',
+        }}
+      >
+        {twitterCards.map((el, index) => {
+          return <TwitterCard key={index} data={el} />;
+        })}
+      </Box>
+      {/* -------------- /Twitter -------------- */}
     </main>
   );
 };
