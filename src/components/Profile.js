7f8a88f9eca9e5d95922b4f34ad8e0ef25aefbce
Aratmany <arat.mda@gmail.com>
2022-12-27 16:28:38 +0300

    fix error ForwardRef

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,19 +1,26 @@
-import { useState } from 'react';
+import { createRef, forwardRef, useState } from 'react';
 import css from '../styles/components/profile.module.scss';
 import Modal from '@mui/material/Modal';
 import Registration from '../ui/Modals/Registration';
 
 export default function Profile() {
   const [open, setOpen] = useState(false);
+  const ref = createRef();
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const RegistrationComponent = forwardRef(({ handleClose }, ref) => {
+    return <Registration innerRef={ref} handleClose={handleClose} />;
+  });
+
   return (
     <>
       <div className={css.main} onClick={handleOpen}>
         profile
       </div>
-      <Modal open={open} onClose={handleClose}>
-        <Registration handleClose={handleClose} />
+      <Modal open={open} onClose={handleClose} keepMounted disableEnforceFocus>
+        <RegistrationComponent handleClose={handleClose} ref={ref} />
       </Modal>
     </>
   );
