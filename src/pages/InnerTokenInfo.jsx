1fd5bfd5bcfbd2cd50ada609d0dc5f773536d98a
Aratmany <arat.mda@gmail.com>
2023-01-02 21:36:41 +0300

    add section LastTopTransactions

--- src/pages/InnerTokenInfo.jsx
+++ src/pages/InnerTokenInfo.jsx
@@ -5,6 +5,7 @@ import TwitterCard from '../ui/TwitterCard/TwitterCard';
 import { useSelector } from 'react-redux';
 import { twitterCardsSelector } from '../store/slices/twitterCards';
 import { ChartTopHolders } from '../ui/ChartTopHolders/ChartTopHolders';
+import { LastTopTransactions } from '../ui/LastTopTransactions/LastTopTransactions';
 
 export const InnerTokenInfo = () => {
   const { twitterCards } = useSelector(twitterCardsSelector.getTwitterCards);
@@ -12,20 +13,23 @@ export const InnerTokenInfo = () => {
     <main className="mt-16 sm:mt-24 ">
       <TokenInfo />
       {/* -------------- Twitter -------------- */}
-      <Box
-        sx={{
-          display: 'flex',
-          overflowX: 'scroll',
-          gap: '20px',
-          padding: '20px 0',
-        }}
-      >
-        {twitterCards.map((el, index) => {
-          return <TwitterCard key={index} data={el} />;
-        })}
-      </Box>
+      {twitterCards.length > 0 && (
+        <Box
+          sx={{
+            display: 'flex',
+            overflowX: 'scroll',
+            gap: '20px',
+            padding: '20px 0',
+          }}
+        >
+          {twitterCards.map((el, index) => {
+            return <TwitterCard key={index} data={el} />;
+          })}
+        </Box>
+      )}
       {/* -------------- /Twitter -------------- */}
       <ChartTopHolders />
+      <LastTopTransactions />
     </main>
   );
 };
