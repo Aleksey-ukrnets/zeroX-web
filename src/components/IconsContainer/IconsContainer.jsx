5e02018d987f76a36a7f805c7299b657f0375ec9
Aratmany <arat.mda@gmail.com>
2023-01-04 18:11:49 +0300

    add icons

--- src/components/IconsContainer/IconsContainer.jsx
+++ src/components/IconsContainer/IconsContainer.jsx
@@ -5,41 +5,108 @@ import {
   faTwitter,
   faDiscord,
   faFacebook,
+  faWeixin,
+  faLinkedin,
+  faGithub,
+  faReddit,
+  faSlack,
 } from '@fortawesome/free-brands-svg-icons';
 import variables from '../../styles/variables.scss';
 
-export const IconsContainer = () => {
+// "bitcointalk": "",
+// "blog": "",
+// "email": "",
+// "website": "https://www.youtube.com/",
+// "whitepaper": ""
+
+// осталось добавить ^ выше элементы
+
+export const IconsContainer = ({ icons }) => {
   return (
     <Grid container spacing={2.5} columns={4}>
-      <Grid item>
-        <FontAwesomeIcon
-          icon={faTelegram}
-          size="xl"
-          style={{ color: variables.colorGrayNeutral }}
-        />
-      </Grid>
+      {icons?.telegram && (
+        <Grid item>
+          <FontAwesomeIcon
+            icon={faTelegram}
+            size="xl"
+            style={{ color: variables.colorGrayNeutral }}
+          />
+        </Grid>
+      )}
+      {icons?.twitter && (
+        <Grid item>
+          <FontAwesomeIcon
+            icon={faTwitter}
+            size="xl"
+            style={{ color: variables.colorGrayNeutral }}
+          />
+        </Grid>
+      )}
+      {icons?.discord && (
+        <Grid item>
+          <FontAwesomeIcon
+            icon={faDiscord}
+            size="xl"
+            style={{ color: variables.colorGrayNeutral }}
+          />
+        </Grid>
+      )}
+      {icons?.facebook && (
+        <Grid item>
+          <FontAwesomeIcon
+            icon={faFacebook}
+            size="xl"
+            style={{ color: variables.colorGrayNeutral }}
+          />
+        </Grid>
+      )}
+      {icons?.wechat && (
+        <Grid item>
+          <FontAwesomeIcon
+            icon={faWeixin}
+            size="xl"
+            style={{ color: variables.colorGrayNeutral }}
+          />
+        </Grid>
+      )}
+      {icons?.linkedin && (
+        <Grid item>
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            size="xl"
+            style={{ color: variables.colorGrayNeutral }}
+          />
+        </Grid>
+      )}
+      {icons?.github && (
+        <Grid item>
+          <FontAwesomeIcon
+            icon={faGithub}
+            size="xl"
+            style={{ color: variables.colorGrayNeutral }}
+          />
+        </Grid>
+      )}
 
-      <Grid item>
-        <FontAwesomeIcon
-          icon={faTwitter}
-          size="xl"
-          style={{ color: variables.colorGrayNeutral }}
-        />
-      </Grid>
-      <Grid item>
-        <FontAwesomeIcon
-          icon={faDiscord}
-          size="xl"
-          style={{ color: variables.colorGrayNeutral }}
-        />
-      </Grid>
-      <Grid item>
-        <FontAwesomeIcon
-          icon={faFacebook}
-          size="xl"
-          style={{ color: variables.colorGrayNeutral }}
-        />
-      </Grid>
+      {icons?.reddit && (
+        <Grid item>
+          <FontAwesomeIcon
+            icon={faReddit}
+            size="xl"
+            style={{ color: variables.colorGrayNeutral }}
+          />
+        </Grid>
+      )}
+
+      {icons?.slack && (
+        <Grid item>
+          <FontAwesomeIcon
+            icon={faSlack}
+            size="xl"
+            style={{ color: variables.colorGrayNeutral }}
+          />
+        </Grid>
+      )}
     </Grid>
   );
 };
