345737ceb369ba8e03d024336f6caa2ada2ebd82
Aratmany <arat.mda@gmail.com>
2023-01-07 20:12:38 +0300

    add slice for marketInfo

--- src/pages/MarketInfo.jsx
+++ src/pages/MarketInfo.jsx
@@ -3,44 +3,73 @@ import React, { useEffect, useState } from 'react';
 import css from '../styles/pages/marketInfo.module.scss';
 import cn from 'classnames';
 import SwiperSlides from '../ui/SwiperSlide/SwiperSlides';
+import { Box, Grid } from '@mui/material';
+import { ChartGainers } from '../ui/ChartGainers/ChartGainers';
+import {
+  actions as actionTopMarketInfo,
+  topMarketInfoSelector,
+} from '../store/slices/topMarketInfo';
+import { useDispatch, useSelector } from 'react-redux';
+import { TableLosers } from '../ui/TableLosers/TableLosers';
 
 export default function MarketInfo() {
-  const [twits, setTwits] = useState([]);
-  const [trades, setTrades] = useState([]);
-  const [topInfoTrades, setTopInfoTrades] = useState([]);
-
+  const dispatch = useDispatch();
   useEffect(() => {
-    axios
-      .get('https://zerox.pro/api/market_info?network=1&part=hype_stat')
-      .then((resp) => {
-        console.log(resp, 'responsesssss');
-        setTwits(resp.data.data.trending_tweets);
-        setTrades(resp.data.data.trending_trades);
+    const fetchTopMarketInfo = async () => {
+      try {
+        const topMarketInfoData = await axios
+          .get('https://zerox.pro/api/market_info?network=1&part=hype_stat')
+          .then((info) => info?.data?.data);
+        dispatch(actionTopMarketInfo.setTopMarketInfo(topMarketInfoData));
+      } catch {
+        console.error('fetchTopMarketInfo: getData error');
+      }
+    };
 
-        setTopInfoTrades(resp.data.data.top_info);
-      });
-  }, []);
+    fetchTopMarketInfo();
+  }, [dispatch]);
 
-  console.log(topInfoTrades);
+  const { topMarketInfo } = useSelector(
+    topMarketInfoSelector.getTopMarketInfoData
+  );
+
+  if (!topMarketInfo.top_info) {
+    return null;
+  }
+
   return (
     <div className={css.market}>
       <div className={css.tradingBlock}>
-        {topInfoTrades.map((el, index) => {
-          let {change_type} = el
+        {topMarketInfo.top_info.map((el, index) => {
+          let { change_type } = el;
           return (
             <div className={css.tradeInfo} key={index}>
               <div className={css.title}>{el.title}</div>
               <div className={css.cost}>${el.volume}</div>
-              <div style={{color: change_type === 'down' ? '#FF0000' : '#75FD53'}}  className={cn(css.persent, css.color)}>{el.change_percent}%</div>
+              <div
+                style={{
+                  color: change_type === 'down' ? '#FF0000' : '#75FD53',
+                }}
+                className={cn(css.persent, css.color)}
+              >
+                {el.change_percent}%
+              </div>
             </div>
           );
         })}
       </div>
       <div className={css.infos}>
-        <SwiperSlides slides={twits} />
-        <SwiperSlides slides={trades} />
+        <SwiperSlides slides={topMarketInfo.trending_tweets} />
+        <SwiperSlides slides={topMarketInfo.trending_trades} />
       </div>
-      <div>text</div>
+      <Grid container spacing={3.75} mt={0}>
+        <Grid item mobile={12} tablet={6}>
+          <ChartGainers />
+        </Grid>
+        <Grid item mobile={12} tablet={6}>
+          <TableLosers />
+        </Grid>
+      </Grid>
     </div>
   );
 }
