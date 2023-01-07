d0be02d2dec3b43553cb652af6e946d615fa4ca1
Aratmany <arat.mda@gmail.com>
2023-01-07 18:58:04 +0300

    add Watch more

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
+import React from 'react';
 import { Box, Grid } from '@mui/material';
 import css from '../../styles/pages/main.module.scss';
 import ProBtn from '../../ui/Buttons/ProBtn';
 import CardToken from '../../ui/CardToken/CardToken';
-export default function CardList({ tokenCards}) {
+export default function CardList({ tokenCards, limit }) {
   return (
     <>
-    <Box
-      mx={{ mobile: 0, tablet: 15 }}
-      px={{ mobile: 0, tablet: 2 }}
-      className={css.cards}
-    >
-      <Grid
-        container
-        spacing={4.75}
-        columns={{ mobile: 6, tablet: 6, desktop: 12 }}
+      <Box
+        mx={{ mobile: 0, tablet: 15 }}
+        px={{ mobile: 0, tablet: 2 }}
+        className={css.cards}
       >
-        {tokenCards.map((el, index) => {
-          return (
-            <Grid item mobile={6} tablet={3} desktop={4} key={index}>
-              <CardToken data={el} />
-            </Grid>
-          );
-        })}
-      </Grid>
-    </Box>
-    <ProBtn />
-  </>
-  )
+        <Grid
+          container
+          spacing={4.75}
+          columns={{ mobile: 6, tablet: 6, desktop: 12 }}
+        >
+          {tokenCards.map((el, index) => {
+            return (
+              <Grid item mobile={6} tablet={3} desktop={4} key={index}>
+                <CardToken data={el} />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Box>
+      <ProBtn />
+    </>
+  );
 }
