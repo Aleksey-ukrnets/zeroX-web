42a2c5b02489ecb00611c4017c49064dd79678f6
Aratmany <arat.mda@gmail.com>
2023-01-09 02:10:45 +0300

    move functions to components

--- src/App.js
+++ src/App.js
@@ -5,37 +5,10 @@ import MarketInfo from './pages/MarketInfo';
 import Pro from './pages/Pro';
 import Container from '@mui/material/Container';
 import { InnerTokenInfo } from './pages/InnerTokenInfo';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { actions as twitterActions } from './store/slices/twitterCards';
-import { actions as tokenActions } from './store/slices/tokenCards';
-import axios from 'axios';
 import { Toaster } from 'react-hot-toast';
+import { Authorization } from './pages/Authorization';
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    //twitterCards
-    axios.get('https://zerox.pro/api/twitter').then((resp) => {
-      dispatch(twitterActions.setTwitterCards([...resp?.data?.data]));
-    });
-    //tokenCards all
-    axios.get('https://zerox.pro/api/token_list?limit=6').then((resp) => {
-      dispatch(tokenActions.setTokenCards([...resp?.data?.data]));
-    });
-    //tokenCards launchpad
-    axios.get('https://zerox.pro/api/token_list?limit=6&filter_type=launchpad').then((resp) => {
-      dispatch(tokenActions.setTokenCardsLaunchpad([...resp?.data?.data]));
-    });
-    //tokenCards algo
-    axios.get('https://zerox.pro/api/token_list?limit=6&filter_type=algo').then((resp) => {
-      dispatch(tokenActions.setTokenCardsAlgo([...resp?.data?.data]));
-    });
-    
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <>
       <Container maxWidth="desktopLarge">
@@ -47,6 +20,7 @@ function App() {
           <Route path="/market" element={<MarketInfo />} />
           <Route path="/pro" element={<Pro />} />
           <Route path="/tokeninfo" element={<InnerTokenInfo />} />
+          <Route path="/auth" element={<Authorization />} />
         </Routes>
       </Container>
       {/* <Footer /> */}
