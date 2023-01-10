e417ccc95b3b53353c9b2860d62c97c9b798738b
Aratmany <arat.mda@gmail.com>
2023-01-10 22:12:40 +0300

    add IconAppStore & IconGooglePlay

---
+++ src/components/Icons/IconGooglePlay.jsx
@@ -0,0 +1,46 @@
+export const IconGooglePlay = ({ height, width }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      fill="none"
+      viewBox="0 0 30 30"
+    >
+      <g clipPath="url(#clip0_1378_115)">
+        <path
+          fill="#fff"
+          fillRule="evenodd"
+          d="M30 6.5v17a6.486 6.486 0 01-6.5 6.5h-17A6.488 6.488 0 010 23.5v-17A6.487 6.487 0 016.5 0h17A6.487 6.487 0 0130 6.5z"
+          clipRule="evenodd"
+        ></path>
+        <g clipPath="url(#clip1_1378_115)">
+          <path
+            fill="#FFC107"
+            d="M24.167 15c0 .625-.35 1.175-.859 1.458l-4.333 2.409L15.308 15l3.667-3.867 4.333 2.409c.509.283.859.833.859 1.458z"
+          ></path>
+          <path
+            fill="#03A9F4"
+            d="M15.308 15l-9 9.492c-.3-.3-.475-.709-.475-1.159V6.667c0-.45.175-.859.475-1.159l9 9.492z"
+          ></path>
+          <path
+            fill="#F44336"
+            d="M15.308 15l3.667 3.867-10.667 5.925A1.667 1.667 0 017.5 25c-.467 0-.892-.192-1.192-.508l9-9.492z"
+          ></path>
+          <path
+            fill="#4CAF50"
+            d="M18.975 11.133L15.308 15l-9-9.492C6.608 5.192 7.033 5 7.5 5c.292 0 .567.075.808.208l10.667 5.925z"
+          ></path>
+        </g>
+      </g>
+      <defs>
+        <clipPath id="clip0_1378_115">
+          <path fill="#fff" d="M0 0H30V30H0z"></path>
+        </clipPath>
+        <clipPath id="clip1_1378_115">
+          <path fill="#fff" d="M0 0H20V20H0z" transform="translate(5 5)"></path>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
